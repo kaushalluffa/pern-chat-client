65168ff5bec4180c111f6ee61efdebe64161c921
kaushalluffa <kaushalluffa91@gmail.com>
2024-02-07 22:34:30 -0500

    fix(styling-with-mui): overall styling is now with mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,8 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { NavigationSidebar } from "./components/navigation/navigation-sidebar";
 
 function App() {
-  return (
-    <div className="bg-white dark:bg-[#313338]">
-      <div className="h-full">
-        <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
-          <NavigationSidebar />
-        </div>
-        <main className="md:pl-[72px] h-full">
-          <Outlet />
-        </main>
-      </div>
-    </div>
-  );
+  return <div>hi</div>;
 }
 
 export default App;
