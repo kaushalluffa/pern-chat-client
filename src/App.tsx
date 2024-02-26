82d6314ed052f595c858787b7b427cc7a00f0f40
kaushalluffa <kaushalluffa91@gmail.com>
2024-02-25 22:05:08 -0500

    removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { Grid } from "@mui/material";
 import React from "react";
-import { Outlet } from "react-router-dom";
 import ResponsiveDrawer from "./components/ResponsiveDrawer/ResponsiveDrawer";
-import SideBarServer from "./components/shared/SideBarServer";
 
 function App() {
   return (
     <Grid container alignItems="center" flexDirection="row">
-      {/* <SideBarServer /> */}
       <ResponsiveDrawer />
     </Grid>
   );
