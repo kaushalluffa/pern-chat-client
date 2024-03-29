c54fa0e6ceeb98767fcb59cc7efd04244a2ba77f
kaushalluffa <kaushalluffa91@gmail.com>
2024-03-29 17:43:54 -0400

    fix

--- src/components/shared/AllRoutes.tsx
+++ src/components/shared/AllRoutes.tsx
@@ -1,8 +1,6 @@
 import App from "@/App";
 import React, { Suspense } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import Signup from "../Auth/Signup";
-import Login from "../Auth/Login";
 import FullScreenLoader from "./FullScreenLoader";
 import Auth from "../Auth/Auth";
 
@@ -13,8 +11,6 @@ const AllRoutes = () => {
         <Route path="/" element={<App />} />
         <Route path="/chat/:chatId" element={<App />} />
         <Route path="/auth" element={<Auth />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Suspense>
